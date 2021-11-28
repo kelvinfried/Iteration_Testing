113e90ba036881697195e1dadea58ecdae4e38a1
Kelvin <Kelvinfriedchicken@gmail.com>
2021-11-28 16:38:37 +0000

    Outputs hello world 5 times.

--- Workshop_4.cpp
+++ Workshop_4.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+	//Print Hello world 5 times
+	for( int x = 0; x < 5; x++ )	//For loop should iterate 5 times; From 0 - 4
+	{
+		std::cout<< "Hello world" << std::endl;	//Output statement
+	}
+	
 	return 0;
 }
